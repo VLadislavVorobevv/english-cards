a108a4630b1b514df940a824ea73c0e5ffdd299e
Vorobev Vladislav <vladislavvorobevvvv@gmail.com>
2025-05-04 16:35:18 +0300

    Первый коммит

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+import Button from './components/Button.vue';
+</script>
+
+<template>
+  <Button>Начать игру</Button>
+</template>
+
+<style scoped></style>
---
+++ src/components/Button.vue
@@ -0,0 +1,27 @@
+<script></script>
+
+<template>
+  <button class="btn"><slot /></button>
+</template>
+
+<style scoped>
+.btn {
+  font-family: var(--font-family);
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 150%;
+  text-align: center;
+
+  background: var(--color-button);
+  color: var(--color-primary);
+  border-radius: 100px;
+  border: none;
+  padding: 16px 10px;
+  min-width: 335px;
+  min-height: 68px;
+}
+
+.btn:hover {
+  background: var(--color-button-hover);
+}
+</style>
